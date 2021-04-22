342b162ae9ed0237f4ba3941efe10e7757dbe6c1
motatoes <moe.habib9@gmail.com>
2021-04-22 19:45:46 +0300

    fix errors

--- main/apigateway.tf
+++ main/apigateway.tf
@@ -1,16 +1,16 @@
 
 resource "aws_api_gateway_rest_api" "main" {
-  name = "${var.project_name}-${var.service_name}"
+  name = "${var.project_name}-${var.environment}"
 }
 
 resource "aws_api_gateway_resource" "proxy" {
-   rest_api_id = aws_api_gateway_rest_api.example.id
-   parent_id   = aws_api_gateway_rest_api.example.root_resource_id
+   rest_api_id = aws_api_gateway_rest_api.main.id
+   parent_id   = aws_api_gateway_rest_api.main.root_resource_id
    path_part   = "{proxy+}"
 }
 
 resource "aws_api_gateway_method" "proxy" {
-   rest_api_id   = aws_api_gateway_rest_api.example.id
+   rest_api_id   = aws_api_gateway_rest_api.main.id
    resource_id   = aws_api_gateway_resource.proxy.id
    http_method   = "ANY"
    authorization = "NONE"
