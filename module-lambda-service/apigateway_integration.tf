eafb4c8f899df41872b3c772bc56213ef1db58ed
motatoes <moe.habib9@gmail.com>
2021-04-30 02:13:47 +0300

    comment out lambda integration

--- module-lambda-service/apigateway_integration.tf
+++ module-lambda-service/apigateway_integration.tf
@@ -1,13 +1,13 @@
 
-resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = var.api_gateway.id
-  parent_id   = var.api_gateway.root_resource_id
-  path_part   = "{proxy+}"
-}
+# resource "aws_api_gateway_resource" "proxy" {
+#   rest_api_id = var.api_gateway.id
+#   parent_id   = var.api_gateway.root_resource_id
+#   path_part   = "{proxy+}"
+# }
 
-resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = var.api_gateway.id
-  resource_id   = aws_api_gateway_resource.proxy.id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
+# resource "aws_api_gateway_method" "proxy" {
+#   rest_api_id   = var.api_gateway.id
+#   resource_id   = aws_api_gateway_resource.proxy.id
+#   http_method   = "ANY"
+#   authorization = "NONE"
+# }
