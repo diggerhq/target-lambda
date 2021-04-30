b60e301f8f6e02274be0fe673b0a0d9db13e59f8
motatoes <moe.habib9@gmail.com>
2021-04-30 03:34:09 +0300

    try to build image in terraform

--- main/ecr.tf
+++ main/ecr.tf
@@ -4,3 +4,9 @@ resource "aws_ecr_repository" "lambda_repo" {
   name                 = "${var.project_name}-${var.environment}"
   image_tag_mutability = var.image_tag_mutability
 }
+
+module "ecr_image" {
+  source = "github.com/byu-oit/terraform-aws-ecr-image?ref=v1.0.1"
+  dockerfile_dir = "lambda-default-image"
+  ecr_repository_url = aws_ecr_repository.lambda_repo.repository_url
+}
