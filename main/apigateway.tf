64eb1f44018f980f76e6975d11ac4b71ad269b05
motatoes <moe.habib9@gmail.com>
2021-05-24 18:21:52 +0300

    set gw name

--- main/apigateway.tf
+++ main/apigateway.tf
@@ -1,5 +1,5 @@
 
-resource "aws_apigatewayv2_api" "example" {
+resource "aws_apigatewayv2_api" "main" {
   name                       = "${var.project_name}-${var.environment}"
   protocol_type              = "WEBSOCKET"
   route_selection_expression = "$request.body.action"
