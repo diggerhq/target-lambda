62179a6e9d1800547ec23cc8c75f2d3b73543a1d
motatoes <moe.habib9@gmail.com>
2021-05-24 17:22:52 +0300

    update gw

--- main/apigateway.tf
+++ main/apigateway.tf
@@ -1,18 +1,6 @@
 
-resource "aws_api_gateway_rest_api" "main" {
-  name = "${var.project_name}-${var.environment}"
+resource "aws_apigatewayv2_api" "example" {
+  name                       = "${var.project_name}-${var.environment}"
+  protocol_type              = "WEBSOCKET"
+  route_selection_expression = "$request.body.action"
 }
-
-# resource "aws_api_gateway_resource" "proxy" {
-#    rest_api_id = aws_api_gateway_rest_api.main.id
-#    parent_id   = aws_api_gateway_rest_api.main.root_resource_id
-#    path_part   = "{proxy+}"
-# }
-
-# resource "aws_api_gateway_method" "proxy" {
-#    rest_api_id   = aws_api_gateway_rest_api.main.id
-#    resource_id   = aws_api_gateway_resource.proxy.id
-#    http_method   = "ANY"
-#    authorization = "NONE"
-# }
-
--- main/roles.tf
+++ main/roles.tf
@@ -13,8 +13,32 @@ resource "aws_iam_role" "default_iam_for_lambda" {
       },
       "Effect": "Allow",
       "Sid": ""
-    }
+    },
+    "Action": [
+        "dynamodb:GetItem",
+        "dynamodb:DeleteItem",
+        "dynamodb:PutItem",
+        "dynamodb:Scan",
+        "dynamodb:Query",
+        "dynamodb:UpdateItem",
+        "dynamodb:BatchWriteItem",
+        "dynamodb:BatchGetItem",
+        "dynamodb:DescribeTable",
+        "dynamodb:ConditionCheckItem"
+    ],
+    "Resource": [
+        "*"
+    ],
+    "Effect": "Allow"
+}
+
+
   ]
 }
 EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_execution_role" {
+  role       = aws_iam_role.default_iam_for_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
