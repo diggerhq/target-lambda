25c2a0a9c7946cbbafa99b36ca74bec4c9cfbcfa
motatoes <moe.habib9@gmail.com>
2021-05-24 21:09:49 +0300

    include role

--- main/roles.tf
+++ main/roles.tf
@@ -47,6 +47,26 @@ resource "aws_iam_policy" "dynamodb" {
     policy = data.aws_iam_policy_document.dynamodb.json
 }
 
+data "aws_iam_policy_document" "gw_connections" {
+    version = "2012-10-17"
+    statement {
+      sid = "ManageGatewayConnections"
+      actions = [
+        "execute-api:ManageConnections"
+      ]
+      effect = "Allow"
+      resources = [
+          aws_apigatewayv2_api.main.arn
+      ]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "gateway_connections" {
+    role       = aws_iam_role.default_iam_for_lambda.name
+    policy_arn = aws_iam_policy.gw_connections.arn
+}
+
+
 resource "aws_iam_role_policy_attachment" "ssm_params" {
     role       = aws_iam_role.default_iam_for_lambda.name
     policy_arn = aws_iam_policy.dynamodb.arn
