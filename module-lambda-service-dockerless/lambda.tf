7246a92a02f0402546be413bf3ac8f22bc3de12b
motatoes <moe.habib9@gmail.com>
2022-01-19 19:16:53 +0000

    add handler

--- module-lambda-service-dockerless/lambda.tf
+++ module-lambda-service-dockerless/lambda.tf
@@ -15,6 +15,7 @@ EOF
 
 resource "aws_lambda_function" "lambda" {
   filename      = "lambda_function.zip"
+  handler       = "index.hanlder"
   function_name = local.function_name
   role          = var.lambda_role
   package_type  = "Zip"
