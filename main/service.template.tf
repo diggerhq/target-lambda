83b9164f1d94667f75f43ecb9e7244998d8c72dc
alexey <alexey@digger.dev>
2022-05-17 14:04:46 +0100

    comment out ecr related var/resources

--- main/service.template.tf
+++ main/service.template.tf
@@ -62,9 +62,11 @@
 {% endif %}
 
 
+        /*
 output "{{service_name}}_docker_registry" {
   value = aws_ecr_repository.lambda_repo.repository_url
 }
+*/
 
 output "{{service_name}}_lb_dns" {
   value = ""
