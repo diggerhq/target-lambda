478720289879898bd0d03e44e228aa7df97694f5
Dias Saparov <dias@digger.dev>
2022-09-14 14:38:13 +0100

    add default tags

--- main/main.template.tf
+++ main/main.template.tf
@@ -44,5 +44,9 @@ provider "aws" {
   access_key = var.aws_key
   secret_key = var.aws_secret
   {% endif %}
-  
+  default_tags {
+    tags = {
+      digger_identifier = "{{aws_app_identifier}}"
+    }
+  }
 }
